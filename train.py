617a2e443a05536fdde26cccfa22ef3c49d42797
Mudhdhoo <104865472+Mudhdhoo@users.noreply.github.com>
2022-05-03 22:24:21 +0200

    Add files via upload

---
+++ train.py
@@ -0,0 +1,6 @@
+from data import *
+from Neural_Network import *
+
+train(20,x_train[0:2000],y_train[0:2000]) # 20 epochs, train on the first 2000 samples
+acc = accuracy(x_test) # Accuracy tested on the test samples
+print(acc)
