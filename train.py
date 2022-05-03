3d26658240a52eb02543ab819420047e5e5f71ee
Mudhdhoo <104865472+Mudhdhoo@users.noreply.github.com>
2022-05-03 22:42:08 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from data import *
+from data import x_test, x_train, y_test, y_train
 from Neural_Network import *
 
 train(20,x_train[0:2000],y_train[0:2000]) # 20 epochs, train on the first 2000 samples
